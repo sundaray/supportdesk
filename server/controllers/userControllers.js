5ced547115a78b69ab1228e0941fb6e7536a8d5b
sundaray <pgcim14.hemant@spjimr.org>
2022-08-05 22:03:20 +0530

    Implemented user registration

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,8 +1,40 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
-const registerUser = asyncHandler(async (req, res) => {
-  res.send(req.body);
+// access: public
+// purpose: register a user
+const registerUser = asyncHandler(async (req, res, next) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  // Check if the user already exists
+
+  const userExists = await User.findOne({ email });
+
+  // If the user exists, then send an error message
+  if (userExists) {
+    const err = new error("User already registered.");
+    err.status = 400;
+    next(err);
+  }
+
+  // if the user does not exist, then create a user and save the user in the database
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  // return the details of the registered user
+  res.json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+  });
 });
+
+// access: public
+// purpose: log in a user
 
 const loginUser = asyncHandler(async (req, res) => {
   res.send("Login user");
