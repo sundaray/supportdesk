28bf0e512aaca83e84467d281883e9035f23cf10
sundaray <pgcim14.hemant@spjimr.org>
2022-08-06 16:56:07 +0530

    Created Home Page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectName } from "./authSlice";
 
 const Home = () => {
   const name = useSelector(selectName);
-  return <>{name && `Welcome ${name}`}</>;
+  return (
+    <>
+      {name && `Welcome ${name}`}
+
+      <section>
+        <h1>What do you need help with?</h1>
+        <p>Please choose from an option below.</p>
+      </section>
+
+      <Link to="new-ticket">Create New Ticket</Link>
+      <Link to="tickets">View my Tickets</Link>
+    </>
+  );
 };
 
 export default Home;
