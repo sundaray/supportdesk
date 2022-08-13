9514c2b19a81bde0a4b2b908ca169fb38120e48d
sundaray <pgcim14.hemant@spjimr.org>
2022-08-13 12:23:38 +0530

    Supportdesk build complete

--- server/index.js
+++ server/index.js
@@ -16,6 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(userRoutes);
 app.use(ticketRoutes);
+
+// Routing logic in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 5000;
